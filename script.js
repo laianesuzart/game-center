bcaa1007ac4b0eefb47b06529799c2c20ddd5dc3
Laiane Suzart <laiane@pm.me>
2021-03-08 16:49:31 -0300

    Add new img for ball

--- script.js
+++ script.js
@@ -122,7 +122,7 @@ const magicBall = document.querySelector(`#magic-ball`);
 const answer = document.querySelector(`#answer`);
 
 const answers = {1: `É certo.`, 2: `Foi decidido assim.`, 3: `Sem dúvidas.`, 4: `Sim, com certeza.`, 5: `A meu ver, sim.`, 6: `Provavelmente.`,
-                 7: `Sim.`, 8: `Sinais apontam que sim.`, 9: `Pergunte mais tarde.`, 10: `É melhor não te contar agora.`, 11: `Concentre-se e pergunte novamente.`,
+                 7: `Sim.`, 8: `Tudo indica que sim.`, 9: `Pergunte mais tarde.`, 10: `É melhor não te contar agora.`, 11: `Concentre-se e pergunte novamente.`,
                  12: `Não conte com isso.`, 13: `Minha resposta é não.`, 14: `Minhas fontes dizem não.`, 15: `Muito duvidoso.`};
 
 const ballAnswers = () => {
@@ -131,27 +131,18 @@ const ballAnswers = () => {
 }
 
 magicBall.addEventListener(`click`, () => {
+    answer.classList.remove(`green`, `yellow`, `red`);
+
     const ballAnswer = ballAnswers();
+
+    if (ballAnswer < 9) {
+        answer.classList.add(`green`);
+    } else if (ballAnswer < 12) {
+        answer.classList.add(`yellow`);
+    } else {
+        answer.classList.add(`red`);
+    }
 
     answer.innerHTML = `${answers[ballAnswer]}`;
 });
 
-
-// ● It is certain.
-// ● It is decidedly so.
-// ● Without a doubt.
-// ● Yes – definitely.
-
-// ● As I see it, yes.
-// ● Most likely.
-// ● Yes.
-// ● Signs point to yes.
-
-// ● Ask again later.
-// ● Better not tell you now.
-// ● Concentrate and ask again.
-
-// ● Don't count on it.
-// ● My reply is no.
-// ● My sources say no.
-// ● Very doubtful.
