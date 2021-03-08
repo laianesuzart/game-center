1d07c11d669922826da56a52c953d757104a90d3
Laiane Suzart <laiane@pm.me>
2021-03-08 12:14:36 -0300

    Add JS for menu

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const menu = document.querySelector(`#menu`);
+const menuList = document.querySelector(`#menu-list`);
+let clickCount = 0;
+
+menu.addEventListener(`click`, () => {
+    
+    if (clickCount === 0) {
+        menuList.classList.remove(`hidden`);
+        clickCount++;
+        console.log(clickCount)
+    } else {
+        menuList.classList.add(`hidden`);
+        clickCount--;
+        console.log(clickCount)
+    }
+    
+    
+})
+
 const game1 = document.querySelector(`#game1`);
 const game2 = document.querySelector(`#game2`);
 const game3 = document.querySelector(`#game3`);
@@ -11,34 +30,46 @@ const words = document.querySelector(`#words`);
 
 
 game1.addEventListener(`click`, () => {
+    menuList.classList.add(`hidden`);
     magic.classList.add(`hidden`);
     nickel.classList.add(`hidden`);
     words.classList.add(`hidden`);
 
     jokempo.classList.remove(`hidden`);
+
+    clickCount--;
  
 });
 
 game2.addEventListener(`click`, () => {
+    menuList.classList.add(`hidden`);
     jokempo.classList.add(`hidden`);
     nickel.classList.add(`hidden`);
     words.classList.add(`hidden`);
 
     magic.classList.remove(`hidden`);
+
+    clickCount--;
 });
 
 game3.addEventListener(`click`, () => {
+    menuList.classList.add(`hidden`);
     magic.classList.add(`hidden`);
     jokempo.classList.add(`hidden`);
     words.classList.add(`hidden`);
 
     nickel.classList.remove(`hidden`);
+
+    clickCount--;
 });
 
 game4.addEventListener(`click`, () => {
+    menuList.classList.add(`hidden`);
     magic.classList.add(`hidden`);
     jokempo.classList.add(`hidden`);
     nickel.classList.add(`hidden`);
  
     words.classList.remove(`hidden`);
+
+    clickCount--;
 });
