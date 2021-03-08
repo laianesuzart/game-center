564cfe09a95aa84fd90ae32c789174b8c9a58ef7
Laiane Suzart <laiane@pm.me>
2021-03-08 15:39:32 -0300

    Add Magic 8-Ball function

--- script.js
+++ script.js
@@ -100,7 +100,7 @@ scissors.addEventListener(`click`, () => {
 });
 
 const computer = () => {
-    const computerChoice = Math.floor(Math.random() * (3)) + 1;
+    const computerChoice = Math.floor(Math.random() * 3) + 1;
     return computerChoice;
 }
 
@@ -117,3 +117,41 @@ const rockPaperScissors = (playerChoice) => {
         result.innerHTML += `<p>Você perdeu :c Tente de novo!</p>`
     }
 }
+
+const magicBall = document.querySelector(`#magic-ball`);
+const answer = document.querySelector(`#answer`);
+
+const answers = {1: `É certo.`, 2: `Foi decidido assim.`, 3: `Sem dúvidas.`, 4: `Sim, com certeza.`, 5: `A meu ver, sim.`, 6: `Provavelmente.`,
+                 7: `Sim.`, 8: `Sinais apontam que sim.`, 9: `Pergunte mais tarde.`, 10: `É melhor não te contar agora.`, 11: `Concentre-se e pergunte novamente.`,
+                 12: `Não conte com isso.`, 13: `Minha resposta é não.`, 14: `Minhas fontes dizem não.`, 15: `Muito duvidoso.`};
+
+const ballAnswers = () => {
+    const ballAnswer = Math.floor(Math.random() * 15) + 1;
+    return ballAnswer;
+}
+
+magicBall.addEventListener(`click`, () => {
+    const ballAnswer = ballAnswers();
+
+    answer.innerHTML = `${answers[ballAnswer]}`;
+});
+
+
+// ● It is certain.
+// ● It is decidedly so.
+// ● Without a doubt.
+// ● Yes – definitely.
+
+// ● As I see it, yes.
+// ● Most likely.
+// ● Yes.
+// ● Signs point to yes.
+
+// ● Ask again later.
+// ● Better not tell you now.
+// ● Concentrate and ask again.
+
+// ● Don't count on it.
+// ● My reply is no.
+// ● My sources say no.
+// ● Very doubtful.
